199557bc9b565b63228ea978266757ca94550d44
btesterc <brkclk.ch@gmail.com>
2024-11-01 14:18:21 +0100

    scope

--- Quiz-App/js/app.js
+++ Quiz-App/js/app.js
@@ -1,7 +1,8 @@
 
  const questionList = [];
+ const KEY = process.env.API
 
- fetch(`https://quizapi.io/api/v1/questions?apiKey=${API}&limit=10`)
+ fetch(`https://quizapi.io/api/v1/questions?apiKey=${KEY}&limit=10`)
     .then((promise) => promise.json())
     .then((data) => {
         console.log(data[0])
